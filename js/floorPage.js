c6229a8576ff4d38304b35b519e3117b26fc242a
bob <301085344@qq.com>
2018-04-15 11:28:48 +0800

    提交代码~~

--- js/floorPage.js
+++ js/floorPage.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		return ruler[0].offsetWidth; 
 	} 
 	function scrollToEnd(){//滚动到底部
-        $('html, body').animate({scrollTop: $(document).height() - $(window).height() - 80}, 300); 
+        $('html, body').animate({scrollTop: $(document).height() - $(window).height() - 120}, 300); 
 	}
 	function shrink(index, subH, subW, theValue, fn){
 		var sub = ".subject[data-index="+index+"]";
